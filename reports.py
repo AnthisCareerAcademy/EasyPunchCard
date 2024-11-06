00cbbfbf23d30b921815db31651a4f8e162b5055
LoganGhast <142341592+Obeth297@users.noreply.github.com>
2024-11-06 12:59:11 -0500

    Copied basic reports function to detailed reports

--- reports.py
+++ reports.py
@@ -25,7 +25,7 @@ class Report():
         current_time = datetime.now()
         current_time = current_time.strftime("%m/%d/%Y %I:%M:%S %p")
         page_num = 1
-        y_pos = 615
+        y_pos = 625
 
         # Set up the canvas
         c = canvas.Canvas(file_name, pagesize=letter)
@@ -59,7 +59,7 @@ class Report():
 
         # Loop through all names and total minutes
         for i, (name, time) in enumerate(zip(data['names'], data['times'])):
-            if y_pos < 50:
+            if y_pos < 70:
                 # Create new page and add page number at the bottom
                 c.showPage()
                 page_num += 1
@@ -79,6 +79,68 @@ class Report():
         # Save the PDF
         c.save()
 
+    def create_detailed_pdf(self, file_name, company_name, title, username, start_date, end_date, data):
+        # Initialize necessary variables
+        current_time = datetime.now()
+        current_time = current_time.strftime("%m/%d/%Y %I:%M:%S %p")
+        page_num = 1
+        y_pos = 625
+
+        # Set up the canvas
+        c = canvas.Canvas(file_name, pagesize=letter)
+
+        # Title
+        c.setFont("Helvetica-Bold", 16)
+        c.drawCentredString(306, 750, company_name)
+        c.setFont("Helvetica-Oblique", 20)
+        c.drawCentredString(306, 720, title)
+
+        # Date
+        c.setFont("Helvetica-Bold", 14)
+        c.drawString(30, 690, f"{start_date} - {end_date}")
+
+        # Username
+        c.setFont("Helvetica-Bold", 12)
+        c.drawString(30, 670, username)
+
+        # Header
+        c.setFont("Helvetica-Bold", 12)
+        c.drawString(75, 650, 'Employee')
+        c.drawString(320, 650, 'Hours:Minutes')
+
+        # Draw the horizontal line below the header
+        c.setStrokeColorRGB(0, 0, 0)
+        c.setLineWidth(2)
+        c.line(65, 645, 525, 645)
+
+        total_pages = self.calculate_pages()
+
+        # First page set up
+        c.setFont("Helvetica", 10)
+        c.drawString(520, 30, f"Page {page_num} of {total_pages}")
+        c.drawString(30, 30, current_time)
+
+        # Loop through all names and total minutes
+        for i, (name, time) in enumerate(zip(data['names'], data['times'])):
+            if y_pos < 70:
+                # Create new page and add page number at the bottom
+                c.showPage()
+                page_num += 1
+                c.setFont("Helvetica", 10)
+                # Page number
+                c.drawString(520, 30, f"Page {page_num} of {total_pages}")
+                # Date/time
+                c.drawString(30, 30, current_time)
+                # Reset y position to top of next page
+                y_pos = 730
+            # Name and total hours:minutes
+            c.setFont("Helvetica", 12)
+            c.drawString(80, y_pos, name)
+            c.drawString(370, y_pos, time)
+            y_pos -= 16
+
+        # Save the PDF
+        c.save()
 
 # Hardcode info for now
 company = "Anthis Cosmetology"
@@ -102,6 +164,6 @@ data = {
 }
 # Create the PDF
 report = Report()
-report.create_basic_pdf("simple_reportlab_pdf.pdf", company, title, start_date, end_date, data)
+report.create_detailed_pdf("simple_reportlab_pdf.pdf", company, title, "ya mama", start_date, end_date, data)
 
 print("PDF created successfully!")
